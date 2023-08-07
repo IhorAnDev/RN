a75b7442996338813e6d0abad93e37d08ec405ec
IhorAnDev <ihorandev@gmail.com>
2023-08-07 14:44:01 +0300

    init commit

--- components/goals/styles/goalComponentStyles.js
+++ components/goals/styles/goalComponentStyles.js
@@ -5,24 +5,21 @@ export const styles = StyleSheet.create({
         paddingTop: 50,
         paddingHorizontal: 16,
         flex: 1,
-        backgroundColor: '#F7F6D8',
     },
     inputContainer: {
-        flexDirection: "row",
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
-        flex: 1
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#311b6b',
     },
     textInput: {
-        padding: 8,
-        paddingVertical: 4,
-        borderWidth: 1,
-        borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8
+        padding: 10,
+        borderColor: '#d7c5ec',
+        backgroundColor: '#d7c5ec',
+        color: '#1923ee',
+        borderRadius: 6,
+        width: '100%',
     },
     listGoalsContainer: {
         flex: 5
@@ -30,10 +27,27 @@ export const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
         borderRadius: 6,
-        backgroundColor: '#5e0acc',
-        padding: 8,
+        backgroundColor: '#761be3',
+
+    },
+    pressedItem: {
+        opacity: 0.5
     },
     goalText: {
         color: 'white',
+        padding: 8,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 16
+    },
+    button: {
+        width: '30%',
+        marginHorizontal: 8
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20
     }
 });
